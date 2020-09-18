7e83b6c4d4fb9f7f7104bf7963af54ac62a2780f
thisissandip <thisissandipmondal@gmail.com>
2020-09-18 17:55:20 +0530

    Updates

---
+++ precache-manifest.4a75461b7d6294c191aded1de4430eb9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecd509d47d8d8d2e9fb2a7b406439b5",
+    "url": "/resume-maker/index.html"
+  },
+  {
+    "revision": "d057b6a2407119e581ad",
+    "url": "/resume-maker/static/css/main.e771c314.chunk.css"
+  },
+  {
+    "revision": "83288b71baad39c7918c",
+    "url": "/resume-maker/static/js/2.96c19e26.chunk.js"
+  },
+  {
+    "revision": "c0456b3ed086187b5a50",
+    "url": "/resume-maker/static/js/3.c9aede21.chunk.js"
+  },
+  {
+    "revision": "82e6361468f5e30ac4d7914780662256",
+    "url": "/resume-maker/static/js/3.c9aede21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7a802a344a6fa5ed2a",
+    "url": "/resume-maker/static/js/4.3eecc6a0.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/resume-maker/static/js/4.3eecc6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d057b6a2407119e581ad",
+    "url": "/resume-maker/static/js/main.bfec93d4.chunk.js"
+  },
+  {
+    "revision": "857b23458ac2ad0cbc2c",
+    "url": "/resume-maker/static/js/runtime-main.81b08dac.js"
+  },
+  {
+    "revision": "4a7e5381f6ec801ad220301c38281328",
+    "url": "/resume-maker/static/media/profile.4a7e5381.jpg"
+  }
+]);
